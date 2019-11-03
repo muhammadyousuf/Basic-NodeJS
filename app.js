766b7a98a208db07d46a84ba9954a5dcad2b7b95
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-11-03 19:32:44 +0500

    add validator

--- app.js
+++ app.js
@@ -9,8 +9,19 @@ fs.appendFileSync("notes.txt", "\nMy Name is Yousuf");
 
 
 */
+
+/*
+-----Lesson three video three
 const { add, name } = require("./utils");
 const notes = require("./notes");
 console.log(notes());
 let sum = add(4, 6);
 console.log(name, " app file", sum);
+
+*/
+
+const validator = require("validator");
+
+console.log(validator.isEmail("muhammadyousuf@327gmail.com"));
+
+console.log(validator.isURL('https://www.facebook.com'))
